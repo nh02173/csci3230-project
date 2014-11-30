f5685e88dc92f24affd5c735f43e5bb634c7bb63
Nicholas Holmes <nh02173@georgiasouthern.edu>
2014-11-29 19:41:27 -0500

    Implemented the algo base class and initial impl of Bubble sort.

--- src/base/modules/Algorithm.java
+++ src/base/modules/Algorithm.java
@@ -1,7 +1,51 @@
 package base.modules;
 
-/**
- * Created by NickSuperb on 11/20/2014.
- */
+import java.util.ArrayList;
+import java.util.Random;
+
 public abstract class Algorithm {
+    public static final int DEFAULT_SIZE = 10;
+    public static final int MINIMUM_SIZE = 10;
+    public static final int DEFAULT_BOUND = 100;
+    public static final int MINIMUM_BOUND = 10;
+
+    public Integer[]Basis;
+
+    public Algorithm(Integer[] input) {
+        if (input.length >= MINIMUM_SIZE) {
+            Basis = input;
+        } else {
+            throw new IllegalArgumentException("Invalid input size, >= " + MINIMUM_BOUND + " is required");
+        }
+    }
+
+    public Algorithm(int sampleSize, int bound) {
+        if (sampleSize >= MINIMUM_SIZE &&
+                bound >= MINIMUM_BOUND) {
+            Basis = getSample(sampleSize, bound);
+        } else {
+            Basis = getSample(DEFAULT_SIZE, DEFAULT_BOUND);
+        }
+    }
+
+    public Algorithm(){
+        this(DEFAULT_SIZE, DEFAULT_BOUND);
+    }
+
+    private Integer[] getSample(int sampleSize, int bound) {
+        Random gen = new Random();
+        ArrayList<Integer> temp = new ArrayList<Integer>(sampleSize);
+
+        for (int index = 0; index < sampleSize; index++) {
+            temp.add(gen.nextInt(bound));
+        }
+        return temp.toArray (new Integer [sampleSize]);
+    }
+
+    void swap(int start, int finish)
+    {
+        int temp = Basis[start];
+        Basis[start] = Basis[finish];
+        Basis[finish] = temp;
+    }
 }
--- src/base/modules/Bubble.java
+++ src/base/modules/Bubble.java
@@ -1,7 +1,33 @@
 package base.modules;
 
-/**
- * Created by NickSuperb on 11/20/2014.
- */
-public class Bubble extends Algorithm {
+import java.util.Queue;
+
+public class Bubble extends Algorithm implements Runnable {
+    public Queue<Integer[]> FrameRecord;
+
+    public Bubble()
+    {
+        super();
+    }
+
+    public Bubble(int size, int bound){
+        super(size, bound);
+    }
+
+    @Override
+    public void runSimulation() {
+        FrameRecord.add(super.Basis.clone());
+        int position, scan;
+
+        for (position = super.Basis.length - 1; position >= 0; position--)
+        {
+            for (scan = 0; scan <= position - 1; scan++)
+            {
+                if (super.Basis[scan].compareTo(super.Basis[scan+1]) > 0) {
+                    super.swap(scan, scan + 1);
+                    FrameRecord.add(super.Basis.clone());
+                }
+            }
+        }
+    }
 }
